89c5d23d6b27a70c27f3d8104ff339e81f35b847
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-14 15:46:58 -0700

    Configure table view data source methods

---
+++ Countries/Countries/MBCountriesTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  MBCountriesTableViewController.h
+//  Countries
+//
+//  Created by Mitchell Budge on 11/14/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MBCountriesTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
